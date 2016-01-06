de057f376bbcb267b42615a9d44b843d94c4f438
juniperphoton <dengweichao@hotmail.com>
2016-01-07 02:00:49 +0800

    Add CategorySetting page.

---
+++ MyerList/UserControl/CategorySettingControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+
+namespace MyerList.UC
+{
+    public sealed partial class CategorySettingControl : UserControl
+    {
+        public CategorySettingControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
